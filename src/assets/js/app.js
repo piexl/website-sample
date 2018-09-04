fbbb8a650c9c30046d8279294ff32e185c6de7d8
yukang <455196886@qq.com>
2018-09-04 17:07:30 +0800

    整理增加新的公用组件

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -9,30 +9,43 @@ var App = function(){
     var app = this;
     //项目配置信息
     app.defeatPageNum = 3; //默认分页数
-    var api_domain = 'http://ush.projects.dragontrail.com/'; //设置示例接口链接
+    var api_domain = 'http://panama.projects.dragontrail.com'; //设置示例接口链接
     app.api = api_domain + '/api/'; //api地址 api_domain 由后端设置在_footer.html中传入
+
+    //打开弹层
+    app.OpenCoverLayer = function () {
+        app.oldSrcollTop = $(window).scrollTop();
+        $('body,html').addClass('un-scroll');
+    }
+    //关闭弹层
+    app.CloseCoverLayer = function () {
+        $('body,html').removeClass('un-scroll');
+        $(window).scrollTop(app.oldSrcollTop);
+    }
+
     /**
      * LoadMoreAjax 发送加载更多ajax请求
      * 
      * @param {boolean}    isLoadingMore  是否为加载更多
      * @param {object}     $elemet        列表jq对象
-     * @param {string}     postType       分类类型
-     * @param {object}     props          传递参数
-     *        @param {string} page        页面
+     * @param {String}     url            请求的url
+     * @param {object}     params         传递参数
+     *        @param {Number} paged         分页id
      * @param {function}   callback       回调函数
-     * @example App.LoadMoreAjax(true, $lsit, 'destination', { page:2, }, function (data) { })
+     * @example
      */
-    app.LoadMoreAjax = function (isLoadingMore, $elemet, postType, props, callback) {
+    app.LoadMoreAjax = function (isLoadingMore, $elemet, url, params, callback) {
         var loading_tpl = '<div class="inline-loading">' +
             '<div class="inline-loading-inner clear-float">' +
-            '<div class="loading-img float-left"><img src="/assets/imgs/loading.gif" alt=""></div>' +
-            '<div class="loading-text float-left">加载中</div>' +
+            '<div class="loading-img float-left"><img src="../assets/imgs/loading.gif" alt=""></div>' +
             '</div>' +
             '</div>';
-        if (isLoadingMore) { $elemet.after(loading_tpl); }
+        if (isLoadingMore) {
+            $elemet.after(loading_tpl);
+        }
         $.ajax({
-            url: app.api + 'mag.' + postType + '.list.json',
-            data: props,
+            url: app.api + url,
+            data: params,
             dataType: 'json',
             success: function (data) {
                 callback && callback(data);
@@ -41,43 +54,48 @@ var App = function(){
                 console.log('error', res);
             },
             complete: function () {
-                if (isLoadingMore) { $('.inline-loading').remove(); }
+                if (isLoadingMore) {
+                    $('.inline-loading').remove();
+                }
             }
         });
     }
+
     /**
      * ListMoreInit 列表滚动加载更多
      * 
-     * @param {object} $list           列表jq对象
-     * @param {string} postType        post类型
+     * @param {object} $list            列表jq对象
+     * @param {string} url              请求的url
+     * @param {object} params           请求的参数
+     *        @param {Number} paged         分页id
      * @param {boolean} listIsAjaxEnd   列表是否加载完
-     * @param {function} callback        回调函数
-     * @example App.ListMoreInit($list，'destination', true, function (data, postType) { })
+     * @param {function} callback       回调函数
+     * @example
      */
-    app.ListMoreInit = function ($list, postType, listIsAjaxEnd, callback) {
-        app.listPage = 2;
+    app.ListMoreInit = function ($list, url, params, listIsAjaxEnd, callback) {
         app.listIsAjax = false;
         app.listIsAjaxEnd = listIsAjaxEnd;
+        app.loadMoreParam = params;
         //滚动加载
         if (!app.isAjaxEnd) {
             $(window).on('scroll', function () {
                 var scrollTop = $(window).scrollTop(),
                     windowHeight = $(window).height(),
                     documentHeight = $(document).height(),
-                    isScrollFooter = scrollTop + windowHeight == documentHeight;
+                    isScrollFooter = scrollTop + windowHeight >= documentHeight;
                 if (isScrollFooter && !app.listIsAjax && !app.listIsAjaxEnd) {
                     app.listIsAjax = true;
-                    app.LoadMoreAjax(true, $list, postType, { page: app.listPage }, function (data) {
+                    app.LoadMoreAjax(true, $list, url, app.loadMoreParam, function (data) {
                         app.listIsAjax = false;
-                        app.listPage++;
-                        callback && callback(data, postType);
+                        app.loadMoreParam.paged++
+                        callback && callback(data, url, params);
                     });
                 }
             });
         }
     }
 
-    app.init = function(){
+    app.Init = function(){
         console.log('app init');
     }();
 }
--- src/assets/js/component.js
+++ src/assets/js/component.js
@@ -29,8 +29,157 @@ var Component = function(){
             $('body,html').animate({ scrollTop: 0 }, 500);
         });
     }
+    //视频弹层
+    component.VideoInit = function(){
+        $('body').on('click', 'a[data-role="video"]', function (event) {
+            event.preventDefault();
+            var $this = $(this),
+                videoUrl = $this.attr('href');
+                poster = $this.data('poster');
+            console.log('videoUrl', videoUrl);
+            if (videoUrl !== '') {
+                var videoTpl = '<div class="com-video-cover" id="videoCover">' +
+                    '<div class="cover-inner">' +
+                    '<div class="inner-box">' +
+                    '<div class="cover-content">' +
+                    '<button class="close-btn"><i class="iconfont icon-close"></i></button>' +
+                    '<video src="' + videoUrl + '" poster="' + poster + '" controls="controls" webkit-playsinline="true" x-webkit-airplay="true" playsinline="true" x5-video-player-type="h5" x5-video-orientation="h5" x5-video-player-fullscreen="true">' +
+                    '</video>' +
+                    '</div>' +
+                    '</div>' +
+                    '</div>' +
+                    '</div>';
+                App.OpenCoverLayer();
+                $('body').append(videoTpl);
+                var $videoCover = $('#videoCover'),
+                    $closeBtn = $videoCover.find('.close-btn'),
+                    thisVideo = $videoCover.find('video')[0],
+                    otherVideo = $('video')[0];
+                if (otherVideo && !otherVideo.paused) {
+                    otherVideo.pause();
+                }
+                if (thisVideo.paused) {
+                    thisVideo.play();
+                }
+                $closeBtn.click(function () {
+                    $videoCover.remove();
+                    App.CloseCoverLayer();
+                });
+            }
+        });
+    }
+    //图片查看器
+    component.GalleryInit = function () {
+        if (component.gallery) {
+            component.gallery.destroy();
+        }
+        $('body').on('click', '[data-role="gallery"]', function (e) {
+            e.stopPropagation();
+            var swipes = $(this).data("gallery"),
+                items = [];
+            photoSwipeTpl = '<div class="pswp pswp-cover" id="photoSwipe" tabindex="-1" role="dialog" aria-hidden="true">' +
+                '<div class="pswp__bg"></div>' +
+                '<div class="pswp__scroll-wrap">' +
+                '<div class="pswp__container">' +
+                '<div class="pswp__item"></div>' +
+                '<div class="pswp__item"></div>' +
+                '<div class="pswp__item"></div>' +
+                '</div>' +
+                '<div class="pswp__ui pswp__ui--hidden">' +
+                '<div class="pswp__top-bar">' +
+                '<div class="pswp__counter"></div>' +
+                '<button class="pswp__button pswp__button--close" title="Close (Esc)"></button>' +
+                '<div class="pswp__preloader">' +
+                '<div class="pswp__preloader__icn">' +
+                '<div class="pswp__preloader__cut">' +
+                '<div class="pswp__preloader__donut"></div>' +
+                '</div>' +
+                ' </div>' +
+                '</div>' +
+                '</div>' +
+                '<div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">' +
+                '<div class="pswp__share-tooltip"></div>' +
+                '</div>' +
+                '<button class="pswp__button pswp__button--arrow--left" title="Previous (arrow left)">' +
+                '</button>' +
+                '<button class="pswp__button pswp__button--arrow--right" title="Next (arrow right)">' +
+                '</button>' +
+                '<div class="pswp__caption">' +
+                '<div class="pswp__caption__center"></div>' +
+                '</div>' +
+                '</div>' +
+                '</div>' +
+                '</div>';
+            $photoSwipe = $(photoSwipeTpl).appendTo('body');
+            // console.log("swipes", swipes);
+            swipes.forEach(function (item) {
+                items.push({
+                    src: item.imageUrl,
+                    w: item.width,
+                    h: item.height
+                })
+            });
+            component.gallery = new PhotoSwipe($('#photoSwipe')[0], PhotoSwipeUI_Default, items, {
+                index: 0, // start at first slide
+                bgOpacity: 0.7
+            });
+            component.gallery.init();
+        })
+    }
+    //搜索
+    component.RearchInit = function () {
+        function checkSearchKeyword(keyword) {
+            if (keyword !== '' && !/^\s*$/g.test(keyword) && keyword !== '.' && !/\//.test(keyword) && !/\\/.test(keyword)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        function search(keyword, $input) {
+            //console.log('keyword', keyword);
+            if (checkSearchKeyword(keyword)) {
+                //window.location.href = "/search/" + keyword;
+            } else {
+                $input.addClass('error');
+            }
+        }
+        $('.search-btn').click(function () {
+            var $this = $(this),
+                $input = $this.siblings('input[data-role="search"]'),
+                keyword = $input.val();
+            search(keyword, $input);
+        });
+        $('input[data-role="search"]').on('keyup', function (event) {
+            var $input = $(this),
+                keyword = $input.val();
+            e = event ? event : (window.event ? window.event : null);
+            var currKey = 0;
+            currKey = e.keyCode || e.which || e.charCode;
+            if (currKey == 13) {
+                search(keyword, $input.parents('.search-inner'));
+            }
+        });
+        $('input[data-role="search"]').on('input', function () {
+            var $input = $(this),
+                isError = $input.hasClass('error'),
+                keyword = $input.val();
+            if (checkSearchKeyword(keyword)) {
+                if (isError) {
+                    $input.removeClass('error');
+                    $input.parents('.search-inner').removeClass('error');
+                }
+            } else {
+                $input.addClass('error');
+                $input.parents('.search-inner').addClass('error');
+            }
+        });
+    }
     component.Init = function(){
         component.GoTopInit();
+        component.VideoInit();
+        component.GalleryInit();
+        component.RearchInit();
         console.log('component init');
     }();
 }
